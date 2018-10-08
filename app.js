ea895c46bec34b6d0991f75f2df5cca616b8fc74
mrn <mrn@srvr.home>
2018-10-08 23:37:44 +0000

    removing white space

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
 
-
-
 // Load routes
 const productsRoutes = require('./api/routes/products');
 
 //app.use() // Handle middleware
-
 // uses the routes we acquired above.
 app.use('/products', productsRoutes);
 
