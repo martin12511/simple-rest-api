15c9b5f6acbe807a8e983270b07c77a7ca0573cb
mrn <mrn@srvr.home>
2018-10-09 11:00:10 +0000

    Adding 404, 500 error handling

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+app.use(morgan('dev'));
 // Load routes
 const productsRoutes = require('./api/routes/products');
 
 //app.use() // Handle middleware
 // uses the routes we acquired above.
 app.use('/products', productsRoutes);
+
+app.use((req,res,next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req,res,next) => {
+	res.status(error.status || 500);
+	res.json({
+		error:{
+			message: error.message,
+		}
+
+	});
+});
+
 
 module.exports = app;
